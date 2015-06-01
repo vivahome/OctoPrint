8489f9dcdff3044217902175a3702e098b26da6d
Volkhard Engelstädter <ve@vivahome.de>
2015-06-01 14:12:29 +0200

    Update Update-System.sh

--- VH-Configs/Update-System.sh
+++ VH-Configs/Update-System.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  cd /var/opt/NetBoxPrint
- sudo /usr/sbin/git pull
+ sudo /usr/bin/git pull
  sudo /var/opt/NetBoxPrint/venv/bin/python setup.py install
  sudo /usr/sbin/service octoprint restart
  cd ~
