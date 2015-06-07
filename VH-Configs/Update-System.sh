40254be714a8a1aba7b4703ef80dd2742bc14d6b
vivahome <ve@vivahome.de>
2015-06-07 16:01:12 +0200

    modify Update-System.sh script

--- VH-Configs/Update-System.sh
+++ VH-Configs/Update-System.sh
@@ -3,6 +3,12 @@
  gitpull=$(sudo /usr/bin/git pull)
  echo "$gitpull"
  ok1="Already up-to-date."
+ # config.log loeschen falls vorhanden
+ if [ -f /tmp/config.log ]
+  then
+  rm -rf /tmp/config.log
+ fi
+ # wenn neue version gezogen wurde dann setup.py aufrufen und octoprint server neu starten 
  if [  "$gitpull" != "$ok1" ]
    then
    sudo /var/opt/NetBoxPrint/venv/bin/python setup.py install
